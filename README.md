33403a69f1579a38d8b5f76153b43b58e672f6c9
v4n <105587619+v4n00@users.noreply.github.com>
2025-01-14 16:25:43 +0200

    feat: added install script

---
+++ README.md
@@ -0,0 +1,7 @@
+# h2mm-cli
+
+install
+
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/v4n00/h2mm-cli/refs/heads/master/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Run me as normal user, not as root."
+    exit 1
+fi
+
+DESTINATION_PATH="/usr/local/bin"
+SCRIPT_NAME="h2mm"
+
+echo "Installing $SCRIPT_NAME..."
+sudo curl https://raw.githubusercontent.com/v4n00/h2mm-cli/refs/heads/master/h2mm --output "$DESTINATION_PATH/$SCRIPT_NAME" 
+sudo chmod +x "$DESTINATION_PATH/$SCRIPT_NAME"
+
+if [[ ! -x "$(command -v $SCRIPT_NAME)" ]]; then
+    echo "Installation failed."
+    exit 1
+fi
+
+echo "Helldivers 2 Mod Manager CLI installed successfully to $DESTINATION_PATH/$SCRIPT_NAME."
